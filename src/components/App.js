02f0b6295d5c5cd0d9c50cc5cdeacb1007a45d01
Bhavik Gosrani <bhavik4748@gmail.com>
2018-04-05 18:50:54 -0400

    multiple components added in App Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,56 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  // state = { counter: 0 };
-  constructor(props) {
-    super(props);
-    this.state = { counter: 9 };
-  }
+class Button extends Component {
+  // state = { counter: 0 }; 
+
+
   render() {
     return (
-
-      <button>{this.state.counter}</button>
-
+      <button onClick={this.props.onClickFunction}> +1 </button>
     );
-  }
+  };
 }
 
 /*
 const App = function (props) {
   return (
     <button>{props.label}  </button>
-  );
+        );
 }
 */
+
+const Result = function (props) {
+  return (
+    <div>
+      {props.counter}
+    </div>
+  );
+
+}
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+    this.incrementCounter = this.incrementCounter.bind(this);
+  };
+
+
+  incrementCounter() {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1
+    }));
+  }
+
+  render() {
+    return (
+      <div >
+        <Button onClickFunction={this.incrementCounter} />
+        <Result counter={this.state.counter} />
+      </div >
+    );
+  }
+}
+
 export default App;
