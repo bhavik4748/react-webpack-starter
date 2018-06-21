aa7bb2898d4bb4ad526ff5285fd9a04fc496898f
Bhavik Gosrani <bhavik4748@gmail.com>
2018-06-21 08:42:25 -0400

    added lodash function
    added lodash function

--- src/components/App2.js
+++ src/components/App2.js
@@ -25,23 +25,28 @@ const Button = function (props) {
 const Answer = function (props) {
     return (
         <div className="col-5">
-            ...
+            <span>5</span>
+              <span>6</span>
         </div>
     );
 }
 
 
 const Numbers = function (props) {
+   
+
     return (
         <div className="card text-center">
             <div>
-                <span>1</span>
-                <span className="selected">2</span>
-                <span className="used">3</span>
+                {Numbers.list.map((number, i) => 
+                <span>{number}</span>
+                )}
             </div>
         </div>
     );
 }
+
+Numbers.list=_.range(1,10);
 
 class Game extends Component {
     render() {
